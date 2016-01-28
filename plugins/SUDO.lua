2296dfa8ff629e4629eff4a62fd209068664b002
parhamhp <parham_hoseini@yahoo.com>
2016-01-28 22:52:25 +0330

    Create SUDO.lua

---
+++ plugins/SUDO.lua
@@ -0,0 +1,19 @@
+do
+
+function run(msg, matches)
+  return [[
+ @MASTERCEC
+
+  end
+return {
+  description = "shows sudoers", 
+  usage = "sudoers : return sudousers",
+  patterns = {
+    "^SUDOERS$",
+    "^sudoers$",
+    "^sudoers$",
+    "^sudoers$",
+  },
+  run = run
+}
+end
